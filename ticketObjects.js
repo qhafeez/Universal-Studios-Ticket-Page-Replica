e6a85e8494f855e52feb92288efc88292f310204
Qasim Hafeez <qhafeez26@yahoo.com>
2017-08-25 12:34:24 -0400

    commiting

---
+++ ticketObjects.js
@@ -0,0 +1,91 @@
+var ticketArray = [
+	
+	{
+		//two park 3-day
+		regPrice: {
+			p2p:{
+				adult:274.99,
+				child:264.99
+			},
+			onePark:{
+				adult:219.99,
+				child:209.99
+			}
+		},
+
+		floridaPrice:{
+			p2p:{
+				adult:199.99,
+				child:190.99
+			},
+			onePark:{
+				adult:159.99,
+				child:150.99
+			}
+		}
+		
+	},
+
+	{
+
+		//3-park 3-day
+		
+		regPrice: {
+			
+			p2p:{
+				adult:314.99,
+				child:304.99
+			},
+			onePark:{
+				adult:259.99,
+				child:249.99
+			}
+		},
+
+		floridaPrice: {
+			
+			p2p:{
+				adult:239.99,
+				child:230.99
+			},
+			onePark:{
+				adult:199.99,
+				child:190.99
+			}
+		}
+		
+	},
+
+	{
+		//2-park 2-day
+		
+		regPrice: {
+			p2p:{
+				adult:254.99,
+				child:249.99
+			},
+			onePark:{
+				adult:199.99,
+				child:189.99
+			}
+		},
+
+		floridaPrice: {
+			
+			p2p:{
+				adult:189.99,
+				child:180.99
+			},
+			onePark:{
+				adult:149.99,
+				child:140.99
+			}
+		}
+
+
+
+		
+	},
+
+];
+
